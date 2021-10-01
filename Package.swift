942a6cd6208d0e13d7e4bbbff53238c460d609bd
marc <mwp1@cornell.edu>
2021-10-01 10:12:42 -0400

    refactor: formatting and naming

--- Package.swift
+++ Package.swift
@@ -3,30 +3,27 @@
 import PackageDescription
 
 let package = Package(
-  name      : "docc2html",
-  platforms : [ .macOS(.v10_14), .iOS(.v11) ],
+  name: "zzdoc",
+  platforms: [ .macOS(.v10_14), .iOS(.v11) ],
   
-  products  : [
-    .library   (name: "DocCHTMLExporter"   , targets: [ "DocCHTMLExporter"   ]),
-    .library   (name: "DocCStaticExporter" , targets: [ "DocCStaticExporter" ]),
-    .executable(name: "docc2html"          , targets: [ "docc2html"          ])
+  products: [
+    .library(name: "DocCHTMLExporter", targets: [ "DocCHTMLExporter" ]),
+    .library(name: "DocCStaticExporter", targets: [ "DocCStaticExporter" ]),
+    .executable(name: "zzdoc", targets: [ "zzdoc" ])
   ],
   
   dependencies: [
-    .package(url  : "https://github.com/marcprux/DocCArchive.git",
-             .branch("main")),
-    .package(url  : "https://github.com/AlwaysRightInstitute/mustache.git",
-             from : "1.0.1"),
-    .package(url  : "https://github.com/apple/swift-log.git",
-             from : "1.4.0")
+    .package(url: "https://github.com/marcprux/DocCArchive.git", .branch("main")),
+    .package(url: "https://github.com/AlwaysRightInstitute/mustache.git", from: "1.0.1"),
+    .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0")
   ],
   
   targets: [
-    .target(name         : "DocCHTMLExporter",
-            dependencies : [ "DocCArchive", "mustache", "Logging" ]),
-    .target(name         : "DocCStaticExporter",
-            dependencies : [ "DocCArchive", "DocCHTMLExporter", "Logging" ]),
-    .target(name         : "zzdoc",
-            dependencies : [ "DocCStaticExporter", "Logging" ])
+    .target(name: "DocCHTMLExporter",
+            dependencies: [ "DocCArchive", "mustache", "Logging" ]),
+    .target(name: "DocCStaticExporter",
+            dependencies: [ "DocCArchive", "DocCHTMLExporter", "Logging" ]),
+    .target(name: "zzdoc",
+            dependencies: [ "DocCStaticExporter", "Logging" ])
   ]
 )
